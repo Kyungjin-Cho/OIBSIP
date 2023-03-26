894c537da90e096f039d24ce893ab0741c7e5e2c
Kyungjin Cho <kyungjin.cho.kr@gmail.com>
2023-03-26 11:23:40 +0100

    Update JLabel
    JLabel for notice, ID, PIN

--- 3_ATM_Interface/src/AtmApp.java
+++ 3_ATM_Interface/src/AtmApp.java
@@ -1,8 +1,9 @@
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.Color;
 
 import javax.swing.JFrame;
-
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class AtmApp {
@@ -46,5 +47,23 @@ public class AtmApp {
     panel.setBackground(new Color(247, 237, 226));
     frame.getContentPane().add(panel);
     panel.setLayout(null);
+
+    JLabel loginNotice = new JLabel("Please enter user ID and user pin.");
+    loginNotice.setFont(new Font("Segoe print", Font.BOLD, 20));
+    loginNotice.setBounds(34, 23, 400, 46);
+    loginNotice.setForeground(new Color(246, 189, 96));
+    panel.add(loginNotice);
+
+    JLabel userID = new JLabel("ID   : ");
+    userID.setFont(new Font("Segoe print", Font.BOLD, 40));
+    userID.setBounds(162, 150, 213, 46);
+    userID.setForeground(new Color(174, 127, 108));
+    panel.add(userID);
+
+    JLabel userPin = new JLabel("PIN : ");
+    userPin.setFont(new Font("Segoe print", Font.BOLD, 40));
+    userPin.setBounds(162, 300, 213, 46);
+    userPin.setForeground(new Color(174, 127, 108));
+    panel.add(userPin);
   }
 }
